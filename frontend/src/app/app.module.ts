9d1df04b5b07977138bf4c21ed86a5029188331e
iacular <github.raluca@xoxy.net>
2024-07-01 13:25:08 +0300

    login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaygroundModule } from './playground/playground.module';
+import { LoginComponent } from './login/login.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';0
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, HomepageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlaygroundModule
+    PlaygroundModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
